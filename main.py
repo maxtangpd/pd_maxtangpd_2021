c989dea939a851804ba5fb1e1cb32a3937917cab
maxtangpd <62405123+maxtangpd@users.noreply.github.com>
2020-03-19 19:38:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import sys
 def one_to_one_map(str1,str2):
     a = set(str1)
     b = set(str2)
-    return(len(str1)==len(str2) and len(a) >= len(b) and len(a)==len(str1))
+    print(len(str1)==len(str2) and len(a) >= len(b) and len(a)==len(str1))
 
-
-
+one_to_one_map(sys.argv[1],sys.argv[2])
